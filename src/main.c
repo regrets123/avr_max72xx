d75122fa05a35c4b9c53b19a0aee6afd1e71962a
Joakim Englund <joakimenglund@protonmail.com>
2026-03-03 14:31:09 +0100

    Initial commit moving_leds_evo

--- src/main.c
+++ src/main.c
@@ -7,13 +7,14 @@ int main() {
 	init_serial();
 	max7219_init();
 
-	int y = 0;
+	int y = 2;
 
 	while (1) {
 		for(int i = 0; i < 16; i++) {
 			printf("%d\n", i);
 
 			max7219b_set(i, y);
+			max7219b_clr((i + 13) % 16 , y); // (i + (16 - 3)) % 16
 			max7219b_out();
 			
 			_delay_ms(500);
